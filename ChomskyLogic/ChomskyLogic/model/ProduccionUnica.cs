655c0577869937995007df71432e72b71180d263
juan17castillo <juancastillo9910@gmail.com>
2019-11-13 20:55:45 -0500

    ProduccionUnica commits

--- ChomskyLogic/ChomskyLogic/model/ProduccionUnica.cs
+++ ChomskyLogic/ChomskyLogic/model/ProduccionUnica.cs
@@ -6,15 +6,29 @@ using System.Threading.Tasks;
 
 namespace ChomskyLogic.model
 {
+
+    ///<summary>
+    ///The ProduccionUnica class is responsible for the business logic of this element of
+    ///grammar for this special case
+    ///</summary>
     public class ProduccionUnica : ICloneable
     {
         public ICollection<Elemento> elements { get; set; }
 
+
+        ///<summary>
+        ///CONSTRUCTOR
+        ///</summary>
         public ProduccionUnica()
         {
             elements = new LinkedList<Elemento>();
-        } 
-
+        }
+        /// <summary>
+        /// This method is in charge of being used to convert to string to anyone who invokes it.
+        /// </summary>
+        /// <returns>
+        /// string type aux
+        /// </returns>
         public String toString()
         {
             String aux = "";
@@ -24,6 +38,12 @@ namespace ChomskyLogic.model
             return aux;
         }
 
+        /// <summary>
+        /// This method is responsible for cloning an item
+        /// </summary>
+        /// <returns>
+        /// object type object
+        /// </returns>
         public object Clone()
         {
             return this.MemberwiseClone();
